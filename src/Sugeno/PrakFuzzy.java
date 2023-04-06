145f334c8385aea52ad96b4dbe95d93e1cddba9a
LuciaDunes <luxiandusis514@gmail.com>
2023-04-06 14:42:36 +0700

    first commit

---
+++ src/Sugeno/PrakFuzzy.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Sugeno;
+
+/**
+ *
+ * @author Bagus_Anggriawan
+ */
+public class PrakFuzzy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
